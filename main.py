53294ead1a1af47c8db4a61f8a92bfd7c1624c33
alex <alexandre.raccurt@gmail.com>
2026-02-23 11:26:12 +0100

    shift function implemented

--- main.py
+++ main.py
@@ -1,6 +1,74 @@
+<<<<<<< HEAD
 import cliText
 import connect
 import interpretCommand
+=======
+#Project by Aurélien Santi - Alexandre Raccurt - Gabriel Zeizer
+
+import socket
+
+host = "vlbeintrocrypto.hevs.ch"
+port = 6000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(host, port)
+
+
+
+
+def messageType(msg):
+    header = b"ISC"
+
+    if msg == "t" or msg == "T" :
+        header += "t"
+    elif msg =="s" or msg == "S":
+        header += "s"
+    elif msg =="i" or msg == "I":
+        header += "i"
+
+    return header
+
+def sizeOfMsg(b):
+    result = b[4:]
+    result = result[:2]
+    return result
+
+def addMsgSize(n): 
+    size = b""
+    size += n.to_bytes(2, byteorder="big")
+
+    return size
+
+
+
+
+def shift(msg, key):
+    res = b""
+    s = int(key)
+
+    for c in msg:
+        if c.isalpha():
+            charInt = int.from_bytes(c.encode("utf-8"), byteorder="big")
+            charInt += s
+            charByte = charInt.to_bytes(4, byteorder="big")
+            res += charByte
+    return res
+        
+
+def decode_shift(msg, key):
+    res = b""
+    s = int(key)
+
+    for c in msg:
+        if c.isalpha():
+            charInt = int.from_bytes(c.encode("utf-8"), byteorder="big")
+            charInt -= s
+            charByte = charInt.to_bytes(4, byteorder="big")
+            res += charByte
+    return res
+    
+
+>>>>>>> e201c94 (shift function implemented)
 
 def main():
     cliText.print1stHeader()
---
+++ test_Shift.py
@@ -0,0 +1,29 @@
+import traceback
+
+def shift(msg, key):
+    res = b""
+    s = int(key)
+
+    for c in msg:
+        charInt = int.from_bytes(c.encode("utf-8"), byteorder="big")
+        charInt += s
+        charByte = charInt.to_bytes(4, byteorder="big")
+        res += charByte
+    return res
+        
+
+def decode_shift(msg, key):
+    res = ""
+    s = int(key)
+
+    for c in range(0, len(msg), 4):
+        chunk = msg[c:c+4]
+        charInt = int.from_bytes(chunk, byteorder="big")
+        charInt -= s
+        res += chr(charInt)
+    return res
+
+test_shift = shift("hello", "123")
+
+print("shifted message ->", shift("hello", "123"))
+print("decoded shifted message ->", decode_shift(test_shift, "123"))
